1fa8c8cd4e138226debf38a111d692a6a2c2fb9e
Jose Tunon <jose.tunon@itxpertix.com>
2022-08-09 22:28:34 -0500

    Respuesta modificada

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const UsuariosController = require("../controllers/user-controller");
 
 router.post("/", UsuariosController.getUser);
-router.get("/", UsuariosController.test);
+router.get("/", (req, res) => res.send('<h1>Logrado</h1>'));
 
 module.exports = router;
